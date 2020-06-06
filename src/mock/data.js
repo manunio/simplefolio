0271c66b1a97cdb6bce519090d7671731372876c
manunio <max.manu.nair@gmail.com>
2020-06-06 18:33:04 +0530

    removed resumemaker link

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://manunio.github.io/simplefolio/#!', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
